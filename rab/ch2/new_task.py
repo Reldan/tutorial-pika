c4d76f317a243578a7228f0e6474646435888a30
Eldar Nugaev <reldan@oscloud.com>
2011-07-17 02:01:52 +0400

    init commit

---
+++ rab/ch2/new_task.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import pika
+import sys
+
+message = ' '.join(sys.argv[1:]) or "Hello World!"
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+channel.queue_declare(queue='task_queue', durable=True)
+channel.basic_publish(exchange='',
+                      routing_key='task_queue',
+                      body=message,
+                      properties=pika.BasicProperties(
+                          delivery_mode = 2,
+                      ))
+print " [x] Sent %r" % (message,)
+connection.close()
